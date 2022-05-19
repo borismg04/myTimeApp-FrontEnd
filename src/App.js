05a4040de802ba3ef0fc4c9f4594957f0df18fe9
borismg04 <borismg04@gmail.com>
2022-05-19 00:02:11 -0500

    creation pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter , Routes ,Route  } from 'react-router-dom';
+import AuthLayout from './layouts/AuthLayout';
+import Login from './pages/Login';
+import Registrar from './pages/Registrar';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout/>}>
+          <Route index element={<Login/>} />
+          <Route path="registrar" element={<Registrar/>} />
+
+        </Route>
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
