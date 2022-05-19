9dc682108ead9a52fbda696be36dfcba4b307c00
borismg04 <borismg04@gmail.com>
2022-05-19 00:39:50 -0500

    style login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Registrar from './pages/Registrar';
 import OlvidePassword from './pages/OlvidePassword';
 import NuevoPassword from './pages/NuevoPassword';
+import ConfirmarCuenta from './pages/ConfirmarCuenta';
 
 import './App.css';
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="registrar" element={<Registrar/>} />
           <Route path="olvide-password" element={<OlvidePassword/>} />
           <Route path="nuevo-password/:token" element={<NuevoPassword/>} />
+          <Route path="confirmar/:id" element={<ConfirmarCuenta/>} />
 
         </Route>
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,43 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">Iniciar Sesión & Administra tus 
+      <span className="text-slate-700"> Proyectos</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+        <div className="my-5">
+          <label className="uppercase text-gray-600 block text-xl font-bold"
+          htmlFor="email"
+          >Email</label>
+          <input
+            id="email"          
+            type="email"
+            placeholder="Ingresa tu Email de Registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-100"
+          />
+        </div>
+        <div className="my-5">
+          <label className="uppercase text-gray-600 block text-xl font-bold"
+          htmlFor="password"
+          >Password</label>
+          <input
+            id="password"          
+            type="password"
+            placeholder="Ingresa tu Password de Registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-100"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Iniciar Sesión"
+          className="w-full bg-sky-700 py-3 text-white uppercase font-bold rounded-xl
+          hover:cursor-pointer hover:bg-sky-800 transiton-colors"
+        />
+
+      </form>
+    </>
   )
 }
 
